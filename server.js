c12d0bc270bb1d167c8232b3826b5dace475c6e0
Josue Majano <josue_majano@yahoo.com>
2020-12-15 22:12:55 -0600

    updated search route to return user first and last name

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
 const { User } = require('./models');
 
 const PORT = 3030;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 //Simple route
 app.get('/', (req, res) => {
@@ -17,13 +18,16 @@ app.get('/', (req, res) => {
 
 //Create new user (runner)
 app.post('/users', async (req, res) => {
-    const { firstName, lastName, email, userName } = req.body;
-    const newUser = await User.createUser({
+    // req.body contains an Object with firstName, lastName, email
+    const { firstName, lastName, userName, email } = req.body;
+    const newUser = await User.create({
         firstName,
         lastName,
-        email,
-        userName
+        userName,
+        email
     });
+    
+    // Send back the new user's ID in the response:
     res.json({
         id: newUser.id
     });
@@ -42,38 +46,17 @@ app.get('/users/by-last-name', async (req, res) => {
 });
 
 app.get('/users/:id', async (req, res) => {
-    const oneUser = await User.findById(req.params.id);
+    const oneUser = await User.findByPk(req.params.id);
     res.json(oneUser);
 })
-
-app.get('/users/:id', async (req, res) => {
-    try{
-        const oneUser = await User.findById(req.params.id);
-        res.json(oneUser);
-    } catch (e) {
-        console.log(e);
-        res.status(404).json({
-            message: 'User not found'
-        });
-    }
-});
-
-app.post('/users/search', async (req, res) => {
-    const users = await User.findAll({
-        where: {
-            firstName: req.body.term,
-        }
-    });
-    res.json(users);
-});
 
 app.post('/users/search', async (req, res) => {
     const users = await User.findAll({
         where: {
             [Sequelize.Op.or]: [
                 { 
-                    firstName: req.body.term,
-                    lastName: req.body.term
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName
                 }
             ]
         }
@@ -104,6 +87,7 @@ app.delete('/users/:id', async (req, res) => {
     res.json(deletedUser);
 });
 
+//
 app.get('*', (req, res) => {
     res.status(404).send('Seems like you' + "'" + "re running the wrong way. Head back to the home page to sign-up/log-in!");
 })
